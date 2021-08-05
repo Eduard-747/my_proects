351f2b84dd95f89d5238a94fce2f0b8ae7a17bad
Eduard-747 <88149958+Eduard-747@users.noreply.github.com>
2021-08-05 21:51:48 +0400

    Add files via upload

---
+++ marathon/mclass.h
@@ -0,0 +1,42 @@
+#include<iostream>
+class contact{
+public:
+    
+    void changeName(std::string name){
+        this->name = name;
+    }
+    
+    void changeFamelName(std::string famelname){
+        this->famelname = famelname;
+    }
+
+    void changePhoneNumber(int phoneNumber){
+        this->phoneNumber = phoneNumber;
+    }
+
+    void changeAddres(std::string addres){
+        this->addres = addres;
+    }
+
+    std::string getName() const {
+        return name;
+    }
+    std::string getFamelName() const {
+        return famelname;
+    }
+    int getPhoneNumber() const {
+        // quantity++;
+        return phoneNumber;
+    }
+    std::string getAddres() const {
+        return addres;
+    }
+
+private:
+
+    std::string name;
+    std::string famelname;
+    int phoneNumber;
+    std::string addres;
+
+};
