1eea58e92b071bb26f638fa304a3ca6e86cbd1b1
Eduard-747 <88149958+Eduard-747@users.noreply.github.com>
2021-10-14 20:32:28 +0400

    Update xnull.cpp

--- XO/xnull.cpp
+++ XO/xnull.cpp
@@ -8,53 +8,54 @@ public:
     }
     void gaming() 
     {
-        int x;
-        int y;
-        char xNull = '-';
-        bool flag = true;
-        bool hert = true;
+        int x ;
+        int y ;
+        char xNull = '-' ;
+        bool flag = true ;
+        bool hert = true ;
         while (flag)
         {
             std::cin >> x >> y ;
-            if(check(x - 1,y - 1)) {
+            if(std::cin.good() == false) {
+                std::cerr << "lav ban ches anum " << std::endl ;
+                exit(1) ;
+            }
+            if(check(x - 1, y - 1) ) {
                 if(hert) {
-                    xNull = 'X';
-                    hert = false;
+                    xNull = 'X' ;
+                    hert = false ;
                 } else {
-                    xNull = 'O';
-                    hert = true;
+                    xNull = 'O' ;
+                    hert = true ;
                 }
-                go(x - 1,y - 1,xNull);
-                flag = checkWin();
+                go(x - 1,y - 1,xNull) ;
+                flag = checkWin() ;
             }
         }
     }
     ~game()
     {
-        std::cout << "\n!!! the game finsh !!!\n";  
+        std::cout << "\n!!! the game finsh !!!\n" ;
         for(int i = 0;i < 3;i++) {
-            delete [] arr[i];
+            delete [] arr[i] ;
         } 
-        delete [] arr;
+        delete [] arr ;
     }
 private:
     void go(int x ,int y,char xNull) 
     {
         if(xNull == 'X' || xNull == 'O') {
-            Gamer obj(x,y,xNull);
-            arr[obj.x][obj.y] = obj;
-        } else {
-            std::cout << "sxal nshan";
-            return;
-        }
-        std::cout << std::endl;
-        print();
+            Gamer obj(x,y,xNull) ;
+            arr[obj.x][obj.y] = obj ;
+        } 
+        std::cout << std::endl ;
+        print() ;
     }
     void print()
     {
         for(int i = 0; i < 3;i++) {
             for(int j = 0; j < 3;j++) {
-                std::cout << "|" << arr[i][j].xNull << "|";
+                std::cout << "|" << arr[i][j].xNull << "|" ;
             }                       
             std::cout << std::endl;
         }
@@ -63,21 +64,23 @@ private:
     bool check(int x,int y) 
     {
         if(x > 2 || x < 0 || y > 2 || y < 0 ) {
-            std::cout << "\nSEGMENTATION\n";
-            return false;
+            std::cout << "\n SEGMENTATION \n" ;
+            return false ;
         }
         if(arr[x][y].xNull == 'X' || arr[x][y].xNull == 'O') {
             std::cout << "inches anum ape!!!!!!!!\n";
-            return false;
+            return false ;
         } else {
-            return true; 
+            return true ;
         }
     }
     bool checkWin() 
     {
         for(int i = 0; i < 3; ++i) {
             for(int j = 0; j < 1;++j) {
+                
                 if(arr[i][j].xNull != '-' && arr[i][j + 1].xNull != '-' && arr[i][j + 2].xNull != '-') {
+
                     if(arr[i][j].xNull == arr[i][j + 1].xNull && arr[i][j + 1].xNull == arr[i][j + 2].xNull) {
                         std::cout << arr[i][j].xNull << " WON ";
                         return false;
@@ -85,6 +88,7 @@ private:
                 }
                 if(i == j) {
                     if(arr[i][j].xNull != '-' && arr[i + 1][j + 1].xNull != '-' && arr[i + 2][j + 2].xNull != '-') {
+
                         if(arr[i][j].xNull == arr[i + 1][j + 1].xNull && arr[i + 1][j + 1].xNull == arr[i + 2][j + 2].xNull) {
                             std::cout << arr[i][j].xNull << " WON ";
                             return false;
@@ -93,6 +97,7 @@ private:
                 }
                 if(i == 3 - j - 1) {
                     if(arr[i][j].xNull != '-' && arr[i - 1][j + 1].xNull != '-' && arr[i - 2][j + 2].xNull != '-') {
+
                         if(arr[i][j].xNull == arr[i - 1][j + 1].xNull && arr[i - 1][j + 1].xNull == arr[i - 2][j + 2].xNull) {
                             std::cout << arr[i][j].xNull << " WON ";
                             return false;
@@ -100,8 +105,9 @@ private:
                     }
                 }
                 if(arr[j][i].xNull != '-' && arr[j + 1][i].xNull != '-' && arr[j + 2][i].xNull != '-') {
+
                     if(arr[j][i].xNull == arr[j + 1][i].xNull && arr[j + 1][i].xNull == arr[j + 2][i].xNull) {
-                        std::cout << arr[j][i].xNull << " WON ";
+                        std::cout << arr[j][i].xNull << " WON " ;
                         return false;
                     }
                 }
@@ -114,25 +120,24 @@ private:
     class Gamer {
     public:
         Gamer() {
-            this->xNull = '-';
+            this->xNull = '-' ;
         }
         Gamer(int x,int y,char xNull) {
-            this->x = x;
-            this->y = y;
-            this->xNull = xNull;
+            this->x = x ;
+            this->y = y ;
+            this->xNull = xNull ;
         }
-        int x;
-        int y;
-        char xNull;
-    };
+        int x ;
+        int y ;
+        char xNull ;
+    } ;
 
 private:
-    Gamer ** arr = new Gamer *[3];
+    Gamer ** arr = new Gamer *[3] ;
 };
 
 int main()
 {
-    
-    game obj;
-    obj.gaming();    
+    game obj ;
+    obj.gaming() ;
 }
